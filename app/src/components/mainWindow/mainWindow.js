32f884e16f6e4c48e10e7b929794e8784a6b3e1b
Nazar Litvin <nazar@qwil.io>
2019-05-20 17:01:04 +0300

    fix scroll

--- app/src/components/mainWindow/mainWindow.js
+++ app/src/components/mainWindow/mainWindow.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import ejs from 'ejs';
 import fetch from 'node-fetch';
 import { camelizeKeys } from 'humps';
 import { BrowserWindow, shell, ipcMain, dialog } from 'electron';
